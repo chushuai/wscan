013e7250098324b350fa364ed2c879e054cb6348
shaochuyu <shaochuyu@360.cn>
2022-08-30 23:59:36 +0800

    【Core.Assassin.Plugins.Helper】添加Knowledge相关的数据结构

--- core/assassin/plugins/helper/knowledge/know.go
+++ core/assassin/plugins/helper/knowledge/know.go
@@ -3,3 +3,24 @@
 * @Date: 5/7/2022 11:30
  */
 package knowledge
+
+import (
+	"context"
+	"go.opencensus.io/resource"
+	"net/http"
+	"sync"
+	"wscan/core/utils/checker/filter"
+)
+
+type KBItem struct {
+	sync.Mutex
+	m map[string]interface{}
+}
+
+type KnowledgeDB struct {
+	rw         sync.RWMutex
+	httpClient *http.Client
+	checkRules []func(context.Context, resource.Resource) error
+	filter     filter.Filter
+	stub       map[string]*KBItem
+}

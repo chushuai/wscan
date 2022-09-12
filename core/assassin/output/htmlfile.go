3e2971d2533753beb04744909c246e70f2234034
shaochuyu <shaochuyu@360.cn>
2022-09-12 15:54:06 +0800

    【Core.Assassin.Output】添加output相关

--- core/assassin/output/htmlfile.go
+++ core/assassin/output/htmlfile.go
@@ -3,3 +3,48 @@
 * @Date: 5/7/2022 11:30
  */
 package output
+
+import (
+	"os"
+	"sync"
+	"wscan/core/utils/printer"
+)
+
+type htmlFilePrinter struct {
+	sync.Mutex
+	filename       string
+	rotateFilename string
+	writer         *os.File
+	writeCount     int
+}
+
+func (*htmlFilePrinter) AddInterceptor(func(interface{}) (interface{}, error)) printer.Printer {
+	return nil
+}
+func (*htmlFilePrinter) Close() error {
+	return nil
+}
+func (*htmlFilePrinter) Lock() {
+
+}
+func (*htmlFilePrinter) LogSubdomain(interface{}) error {
+	return nil
+}
+func (*htmlFilePrinter) LogVuln(interface{}) error {
+	return nil
+}
+func (*htmlFilePrinter) Print(interface{}) error {
+	return nil
+}
+func (*htmlFilePrinter) Unlock() {
+
+}
+func (*htmlFilePrinter) lockSlow() {
+
+}
+func (*htmlFilePrinter) unlockSlow(int32) {
+
+}
+func (*htmlFilePrinter) writePrefix() error {
+	return nil
+}
--- core/assassin/output/webhook.go
+++ core/assassin/output/webhook.go
@@ -4,9 +4,45 @@
  */
 package output
 
+import (
+	"context"
+	"net/url"
+	"wscan/core/assassin/model"
+	"wscan/core/utils/printer"
+)
+
 func NewWebHookPrinter() {
 
 }
 
 type webHookPrinter struct {
+	ctx    context.Context
+	url    *url.URL
+	header map[string][]string
+	client *http.Client
+}
+
+func (*webHookPrinter) AddInterceptor(func(interface{}) (interface{}, error)) printer.Printer {
+	return nil
+}
+func (*webHookPrinter) Close() error {
+	return nil
+}
+func (*webHookPrinter) LogStats(*model.StatisticRecord) error {
+	return nil
+}
+func (*webHookPrinter) LogSubdomain(*model.SubDomainResult) error {
+	return nil
+}
+func (*webHookPrinter) LogVuln(*model.Vuln) error {
+	return nil
+}
+func (*webHookPrinter) Print(interface{}) error {
+	return nil
+}
+func (*webHookPrinter) sendReq() {
+
+}
+func (*webHookPrinter) truncResult([]uint8) []uint8 {
+	return nil
 }

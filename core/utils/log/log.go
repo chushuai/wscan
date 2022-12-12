262fbbd991e2828d186e1cf15d27b67daded8cf8
shaochuyu <shaochuyu@360.cn>
2022-12-12 18:02:37 +0800

    【utils.logs】修改日志模块

--- core/utils/log/log.go
+++ core/utils/log/log.go
@@ -20,16 +20,66 @@ func GetLogger(name string) *Logger {
 	return &Logger{name: name, Logger: golog.New()}
 }
 
+func Debug(v ...interface{}) {
+	defaultLogger.Debug(v...)
+}
+
+func Info(v ...interface{}) {
+	defaultLogger.Info(v...)
+}
+
+func Warning(v ...interface{}) {
+	defaultLogger.Warn(v...)
+}
+
+func Warn(v ...interface{}) {
+	defaultLogger.Warn(v...)
+}
+
+func Error(v ...interface{}) {
+	defaultLogger.Error(v...)
+}
+
+func Fatal(v ...interface{}) {
+	defaultLogger.Fatal(v...)
+}
+
+func Println(v ...interface{}) {
+	defaultLogger.Println(v...)
+}
+
+func Print(v ...interface{}) {
+	defaultLogger.Print(v...)
+}
+
+func Printf(format string, args ...interface{}) {
+	defaultLogger.Printf(format, args...)
+}
+
 func Infof(format string, args ...interface{}) {
-	defaultLogger.Logf(golog.InfoLevel, format, args...)
+	defaultLogger.Infof(format, args...)
 }
 
 func Debugf(format string, args ...interface{}) {
-	defaultLogger.Logf(golog.InfoLevel, format, args...)
+	defaultLogger.Debugf(format, args...)
+}
+
+func Warningf(format string, args ...interface{}) {
+	defaultLogger.Warningf(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	defaultLogger.Warnf(format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	defaultLogger.Errorf(format, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	defaultLogger.Fatalf(format, args...)
 }
 
 func init() {
 	defaultLogger = GetLogger("default")
-	golog.New()
-
 }

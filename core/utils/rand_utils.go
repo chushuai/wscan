1a7ce2b39827ce55f09fd6697aabd95688c95e91
shaochuyu <shaochuyu@360.cn>
2023-03-15 23:42:39 +0800

    【Core.Utils】实现RandStringBytes方法

--- core/utils/rand_utils.go
+++ core/utils/rand_utils.go
@@ -3,3 +3,22 @@
 * @Date: 5/7/2022 11:30
  */
 package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandStringBytes(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	rand.Seed(time.Now().UnixNano())
+
+	// 创建一个切片，其中包含所有可用的字母
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+
+	// 将字节数组转换为字符串并返回
+	return string(b)
+}

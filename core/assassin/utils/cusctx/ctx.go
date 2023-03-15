d46128c62130edf057364c7b5f5fc605ca8442de
shaochuyu <shaochuyu@360.cn>
2023-03-16 00:35:26 +0800

    【Core.Assassin.Utils.Cusctx】实现WithSignal

--- core/assassin/utils/cusctx/ctx.go
+++ core/assassin/utils/cusctx/ctx.go
@@ -3,3 +3,28 @@
 * @Date: 5/7/2022 11:30
  */
 package cusctx
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func WithSignal(parent context.Context, sig syscall.Signal) (context.Context, func()) {
+	ctx, cancel := context.WithCancel(parent)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, sig)
+
+	go func() {
+		defer signal.Stop(c)
+		select {
+		case <-c:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	return ctx, cancel
+}

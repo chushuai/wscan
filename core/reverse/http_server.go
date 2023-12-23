38393f09aeb053388bac04c8b21992177456057f
shaochuyu <shaochuyu@360.cn>
2023-12-23 18:53:05 +0800

    【Reverse】HTTP返连平台

--- core/reverse/http_server.go
+++ core/reverse/http_server.go
@@ -3,3 +3,340 @@
 * @Date: 5/7/2022 11:30
  */
 package reverse
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+	logger "wscan/core/utils/log"
+)
+
+type HTTPServer struct {
+	Server                *http.Server
+	Router                *httprouter.Router
+	config                *Config
+	db                    *DB
+	internalGroupEventMap *sync.Map
+}
+
+type RespData struct {
+	HTTPStatusCode string
+	Msg            string
+}
+
+type BulkRespData struct {
+	HTTPStatusCode string
+	Msg            []string
+}
+
+type queryInfo struct {
+	Query string
+}
+
+func JsonRespData(resp interface{}) string {
+	rs, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return string(rs)
+}
+
+func (self *HTTPServer) VerifyToken(token string) bool {
+	flag := false
+	if token == self.config.Token {
+		flag = true
+	}
+	return flag
+}
+
+func (self *HTTPServer) GetUser(domain string) string {
+	user := "admin"
+	//for i, v := range Config.HTTP.User {
+	//	if strings.Contains(domain, v) {
+	//		user = i
+	//		break
+	//	}
+	//}
+	return user
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/template", http.StatusMovedPermanently)
+}
+
+func (self *HTTPServer) GetDnsData(w http.ResponseWriter, r *http.Request) {
+	key := r.Header.Get("token")
+	if self.VerifyToken(key) {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "200",
+			Msg:            D.Get(key),
+		}))
+	} else {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "403",
+			Msg:            "false",
+		}))
+	}
+}
+
+func (self *HTTPServer) verifyTokenApi(w http.ResponseWriter, r *http.Request) {
+	var data map[string]string
+	token, _ := io.ReadAll(r.Body)
+	json.Unmarshal(token, &data)
+	if self.VerifyToken(data["token"]) {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "200",
+			Msg:            "dnslog.demon.cn", //Core.Config.HTTP.User[data["token"]] + "." + Core.Config.DNS.Domain,
+		}))
+	} else {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "403",
+			Msg:            "false",
+		}))
+	}
+}
+
+func (self *HTTPServer) JsonRespData(resp interface{}) string {
+	rs, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return string(rs)
+}
+
+func (self *HTTPServer) Clean(w http.ResponseWriter, r *http.Request) {
+	key := r.Header.Get("token")
+	if self.VerifyToken(key) {
+		D.Clear(key)
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "200",
+			Msg:            "success",
+		}))
+	} else {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "403",
+			Msg:            "false",
+		}))
+	}
+}
+
+func (self *HTTPServer) verifyDns(w http.ResponseWriter, r *http.Request) {
+	DnsDataRwLock.RLock()
+	defer DnsDataRwLock.RUnlock()
+	var Q queryInfo
+	key := r.Header.Get("token")
+	if self.VerifyToken(key) {
+		body, _ := io.ReadAll(r.Body)
+		json.Unmarshal(body, &Q)
+		resp := RespData{
+			HTTPStatusCode: "200",
+			Msg:            "false",
+		}
+		for _, v := range DnsData[key] {
+			if v.Subdomain == Q.Query {
+				resp.Msg = "true"
+				break
+			}
+
+		}
+		fmt.Fprint(w, JsonRespData(resp))
+	} else {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "403",
+			Msg:            "false",
+		}))
+	}
+}
+
+func (self *HTTPServer) verifyHttp(w http.ResponseWriter, r *http.Request) {
+	DnsDataRwLock.RLock()
+	defer DnsDataRwLock.RUnlock()
+	var Q queryInfo
+	key := r.Header.Get("token")
+	if self.VerifyToken(key) {
+		body, _ := io.ReadAll(r.Body)
+		json.Unmarshal(body, &Q)
+		resp := RespData{
+			HTTPStatusCode: "200",
+			Msg:            "false",
+		}
+		for _, v := range DnsData[key] {
+			if v.Subdomain == Q.Query && v.Type == "HTTP" {
+				resp.Msg = "true"
+				break
+			}
+
+		}
+		fmt.Fprint(w, JsonRespData(resp))
+	} else {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "403",
+			Msg:            "false",
+		}))
+	}
+}
+
+func (self *HTTPServer) BulkVerifyDns(w http.ResponseWriter, r *http.Request) {
+	DnsDataRwLock.RLock()
+	defer DnsDataRwLock.RUnlock()
+	var Q []string
+	key := r.Header.Get("token")
+	if self.VerifyToken(key) {
+		body, _ := io.ReadAll(r.Body)
+		json.Unmarshal(body, &Q)
+		var result []string
+		for _, v := range DnsData[key] {
+			for _, q := range Q {
+				if v.Subdomain == q {
+					result = append(result, q)
+				}
+			}
+		}
+		var resp BulkRespData
+		if len(result) == 0 {
+			resp = BulkRespData{
+				HTTPStatusCode: "200",
+				Msg:            result,
+			}
+		} else {
+			resp = BulkRespData{
+				HTTPStatusCode: "200",
+				Msg:            removeDuplication(result),
+			}
+		}
+		fmt.Fprint(w, JsonRespData(resp))
+	} else {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "403",
+			Msg:            "false",
+		}))
+	}
+}
+
+func removeDuplication(arr []string) []string {
+	if arr == nil || len(arr) == 0 {
+		return []string{}
+	}
+	j := 0
+	for i := 1; i < len(arr); i++ {
+		if arr[i] == arr[j] {
+			continue
+		}
+		j++
+		arr[j] = arr[i]
+	}
+	return arr[:j+1]
+}
+
+func (self *HTTPServer) BulkVerifyHttp(w http.ResponseWriter, r *http.Request) {
+	DnsDataRwLock.RLock()
+	defer DnsDataRwLock.RUnlock()
+	var Q []string
+	key := r.Header.Get("token")
+	if self.VerifyToken(key) {
+		body, _ := io.ReadAll(r.Body)
+		json.Unmarshal(body, &Q)
+		var result []string
+		for _, v := range DnsData[key] {
+			for _, q := range Q {
+				if v.Subdomain == q && v.Type == "HTTP" {
+					result = append(result, q)
+				}
+			}
+		}
+		var resp BulkRespData
+		if len(result) == 0 {
+			resp = BulkRespData{
+				HTTPStatusCode: "200",
+				Msg:            result,
+			}
+		} else {
+			resp = BulkRespData{
+				HTTPStatusCode: "200",
+				Msg:            removeDuplication(result),
+			}
+		}
+		fmt.Fprint(w, JsonRespData(resp))
+	} else {
+		fmt.Fprint(w, JsonRespData(RespData{
+			HTTPStatusCode: "403",
+			Msg:            "false",
+		}))
+	}
+}
+
+func (self *HTTPServer) removeDuplication(arr []string) []string {
+	if arr == nil || len(arr) == 0 {
+		return []string{}
+	}
+	j := 0
+	for i := 1; i < len(arr); i++ {
+		if arr[i] == arr[j] {
+			continue
+		}
+		j++
+		arr[j] = arr[i]
+	}
+	return arr[:j+1]
+}
+
+func (self *HTTPServer) isIpaddress(ip string) bool {
+	return net.ParseIP(ip) != nil
+}
+
+func isIpaddress(ip string) bool {
+	return net.ParseIP(ip) != nil
+}
+
+func (self *HTTPServer) HttpRequestLog(w http.ResponseWriter, r *http.Request) {
+	user := self.GetUser(r.URL.Path)
+	clientIp := r.RemoteAddr
+	xip := r.Header.Get("X-Forwarded-For")
+	if xip != "" && isIpaddress(xip) {
+		clientIp = xip
+	}
+	D.Set(user, DnsInfo{
+		Type:      "HTTP",
+		Subdomain: r.URL.Path,
+		Ipaddress: clientIp,
+		Time:      time.Now().Unix(),
+	})
+}
+
+//go:embed template
+var template embed.FS
+
+func (self *HTTPServer) ListingHttpManagementServer() {
+	mux := http.NewServeMux()
+	mux.Handle("/template/", http.FileServer(http.FS(template)))
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/api/verifyToken", self.verifyTokenApi)
+	mux.HandleFunc("/api/getDnsData", self.GetDnsData)
+	mux.HandleFunc("/api/Clean", self.Clean)
+	mux.HandleFunc("/api/verifyDns", self.verifyDns)
+	mux.HandleFunc("/api/bulkVerifyDns", self.BulkVerifyDns)
+	mux.HandleFunc("/api/verifyHttp", self.verifyHttp)
+	mux.HandleFunc("/api/BulkVerifyHttp", self.BulkVerifyHttp)
+	mux.HandleFunc("/"+"dnslog"+"/", self.HttpRequestLog)
+	server := &http.Server{
+		Addr:    ":" + self.config.HTTPServerConfig.ListenPort,
+		Handler: mux,
+	}
+	logger.Info("Http address: http://" + "0.0.0.0:" + self.config.HTTPServerConfig.ListenPort)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewHTTPServer(cfg *Config) *HTTPServer {
+	return &HTTPServer{
+		config: cfg,
+	}
+}
--- core/reverse/reverse.go
+++ core/reverse/reverse.go
@@ -5,44 +5,10 @@
 package reverse
 
 import (
-	"github.com/miekg/dns"
-	"go.etcd.io/bbolt"
+	"encoding/json"
 	"golang.org/x/net/context"
-	"io"
-	"net"
-	"net/http"
 	"sync"
-	"time"
 )
-
-type DB struct {
-	sync.Mutex
-	path     string
-	isTempDB bool
-	*bbolt.DB
-}
-
-type DNSServer struct {
-	*dns.Server
-	config                *Config
-	db                    *DB
-	internalGroupEventMap *sync.Map
-}
-
-type HTTPServer struct {
-	Server *http.Server
-	//Router                *httprouter.Router
-	config                *Config
-	db                    *DB
-	internalGroupEventMap *sync.Map
-}
-
-type RMIServer struct {
-	listener              net.Listener
-	config                *Config
-	db                    *DB
-	internalGroupEventMap *sync.Map
-}
 
 type Reverse struct {
 	ctx                   context.Context
@@ -57,31 +23,354 @@ type Reverse struct {
 	groupToDelete         remoteFetchEventRequest
 }
 
-type Unit struct {
-	sync.Mutex
-	// reverse *<nil>
-	id string
-	// group *<nil>
-	Callback func(*Event) error
-	Data     interface {
+func NewReverse(config *Config) *Reverse {
+	r := &Reverse{config: config,
+		reverseHTTPServer: NewHTTPServer(config)}
+
+	return r
+}
+
+func (r *Reverse) Start() {
+	r.reverseHTTPServer.ListingHttpManagementServer()
+
+}
+
+func (r *Reverse) Close() error {
+	return nil
+}
+
+func (r *Reverse) Config() *Config {
+	return r.config
+}
+
+var DnsData = make(map[string][]DnsInfo)
+
+var DnsDataRwLock sync.RWMutex
+
+type DnsInfo struct {
+	Type      string
+	Subdomain string
+	Ipaddress string
+	Time      int64
+}
+
+func (d *DnsInfo) Set(token string, data DnsInfo) {
+	DnsDataRwLock.Lock()
+	defer DnsDataRwLock.Unlock()
+	if DnsData[token] == nil {
+		DnsData[token] = []DnsInfo{data}
+	} else {
+		DnsData[token] = append(DnsData[token], data)
 	}
 }
 
-type UnitGroup struct {
-	id             string
-	units          sync.Map
-	callbackCalled int32
-	expireAt       time.Time
+func (d *DnsInfo) Get(token string) string {
+	DnsDataRwLock.RLock()
+	defer DnsDataRwLock.RUnlock()
+	res := ""
+	if DnsData[token] != nil {
+		v, _ := json.Marshal(DnsData[token])
+		res = string(v)
+	}
+	if res == "" {
+		res = "null"
+	}
+	return res
 }
 
-type peekedConn struct {
-	net.Conn
-	r io.Reader
+func (d *DnsInfo) Clear(token string) {
+	DnsData[token] = []DnsInfo{}
+	DnsData["other"] = []DnsInfo{}
 }
 
-type rmiHTTPListener struct {
-	net.Listener
-	token                 string
-	db                    *DB
-	internalGroupEventMap *sync.Map
+var D DnsInfo
+
+/*
+
+var D DnsInfo
+
+*/
+/*
+
+func (r *Reverse) FetchEvent() {
+	// Use context to control the overall execution time
+	ctx, cancel := context.WithTimeout(r.ctx, 30*time.Second)
+	defer cancel()
+
+	// Fetch events locally
+	r.localFetchEvent()
+
+	// Fetch events remotely
+	err := r.remoteFetchEvent()
+	if err != nil {
+		// Handle the error (log, return, or take appropriate action)
+		fmt.Printf("Error fetching remote events: %v\n", err)
+	}
 }
+func (*Reverse) NewUnitGroup() *UnitGroup {
+	return nil
+}
+func (*Reverse) Register(unitInterface interface{}) *Unit {
+	// Check if the provided interface is of type *Unit
+	unit, ok := unitInterface.(*Unit)
+	if !ok {
+		// Handle the error (log, return, or take appropriate action)
+		fmt.Println("Invalid unit interface provided.")
+		return nil
+	}
+
+	// Optionally, perform additional validations or checks on the unit
+	// ...
+
+	// Register the unit
+	// You might want to use a unique identifier for each unit
+	unitID := generateUniqueID() // Replace with your actual method for generating IDs
+	r.groupUnitCallbackMap.Store(unitID, unit)
+
+	// Optionally, perform additional actions after registration
+	// ...
+
+	return unit
+}
+func (r *Reverse) RegisterWithGroup(unitInterface interface{}, group *UnitGroup) *Unit {
+	// Check if the provided interface is of type *Unit
+	unit, ok := unitInterface.(*Unit)
+	if !ok {
+		// Handle the error (log, return, or take appropriate action)
+		fmt.Println("Invalid unit interface provided.")
+		return nil
+	}
+
+	// Check if the provided group is nil
+	if group == nil {
+		// Handle the error (log, return, or take appropriate action)
+		fmt.Println("Group cannot be nil.")
+		return nil
+	}
+
+	// Add the unit to the specified group
+	group.AddUnit(unit)
+
+	// Optionally, you can perform additional actions or validations here
+
+	return unit
+	return nil
+}
+func (r *Reverse) gcExpiredEventMap() {
+	// Get the current time
+	currentTime := time.Now()
+
+	// Iterate over the items in the internal group event map
+	r.internalGroupEventMap.Range(func(key, value interface{}) bool {
+		// Assuming that the value is a slice of events ([]*Event)
+		events, ok := value.([]*Event)
+		if !ok {
+			// Handle the unexpected value type (log, return, or take appropriate action)
+			fmt.Println("Unexpected value type in internal group event map.")
+			return true
+		}
+
+		// Filter out expired events
+		filteredEvents := make([]*Event, 0, len(events))
+		for _, event := range events {
+			// Assuming that Event has an 'ExpirationTime' field
+			if event.ExpirationTime.After(currentTime) {
+				filteredEvents = append(filteredEvents, event)
+			}
+		}
+
+		// Update the value in the map with the filtered events
+		r.internalGroupEventMap.Store(key, filteredEvents)
+
+		return true
+	})
+}
+func (r *Reverse) gcExpiredGroup() {
+	// Get the current time
+	//currentTime := time.Now()
+	//
+	//// Iterate over the groupUnitCallbackMap
+	//r.groupUnitCallbackMap.Range(func(key, value interface{}) bool {
+	//	// Assuming that the value is a UnitGroup
+	//	group, ok := value.(*UnitGroup)
+	//	if !ok {
+	//		// Handle the unexpected value type (log, return, or take appropriate action)
+	//		fmt.Println("Unexpected value type in groupUnitCallbackMap.")
+	//		return true
+	//	}
+	//
+	//	// Assuming that UnitGroup has an 'ExpirationTime' field
+	//	if group.ExpirationTime.Before(currentTime) {
+	//		// The group has expired, remove it from the map
+	//		r.groupUnitCallbackMap.Delete(key)
+	//		fmt.Printf("Expired group deleted: %v\n", key)
+	//	}
+	//
+	//	return true
+	//})
+}
+func (*Reverse) healthCheck(context.Context) error {
+	return nil
+}
+
+func (r *Reverse) launchServer() error {
+	// Launch HTTP server
+	httpErrCh := make(chan error)
+	go func() {
+		err := r.reverseHTTPServer.Server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			httpErrCh <- err
+		}
+		close(httpErrCh)
+	}()
+
+	// Launch DNS server
+	dnsErrCh := make(chan error)
+	go func() {
+		err := r.reverseDNSServer.ListenAndServe()
+		if err != nil {
+			dnsErrCh <- err
+		}
+		close(dnsErrCh)
+	}()
+
+	// Launch RMI server
+	rmiErrCh := make(chan error)
+	go func() {
+		err := r.reverseRMIServer.Serve()
+		if err != nil {
+			rmiErrCh <- err
+		}
+		close(rmiErrCh)
+	}()
+
+	// Wait for any server to return an error or for the context to be canceled
+	select {
+	case err := <-httpErrCh:
+		return fmt.Errorf("HTTP server error: %v", err)
+	case err := <-dnsErrCh:
+		return fmt.Errorf("DNS server error: %v", err)
+	case err := <-rmiErrCh:
+		return fmt.Errorf("RMI server error: %v", err)
+	case <-r.ctx.Done():
+		// Context canceled, shut down the servers gracefully
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := r.reverseHTTPServer.Server.Shutdown(shutdownCtx); err != nil {
+			fmt.Println("Error shutting down HTTP server:", err)
+		}
+
+		if err := r.reverseDNSServer.Shutdown(shutdownCtx); err != nil {
+			fmt.Println("Error shutting down DNS server:", err)
+		}
+
+		r.reverseRMIServer.Close()
+
+		return nil
+	}
+}
+
+func (r *Reverse) localCallUnitCallback() {
+	// Iterate over the units in the groupUnitCallbackMap
+	r.groupUnitCallbackMap.Range(func(key, value interface{}) bool {
+		// Assuming that the value is a *Unit
+		unit, ok := value.(*Unit)
+		if !ok {
+			// Handle the unexpected value type (log, return, or take appropriate action)
+			fmt.Println("Unexpected value type in groupUnitCallbackMap.")
+			return true
+		}
+
+		// Call the callback function of the unit
+		unit.Callback()
+
+		// Optionally, perform additional actions after calling the callback
+		// ...
+
+		return true
+	})
+}
+func (r *Reverse) localFetchEvent() {
+	// Assuming DB is a BoltDB database
+	// You need to replace it with your actual local storage implementation
+	r.db.Lock()
+	defer r.db.Unlock()
+
+	err := r.db.View(func(tx *bbolt.Tx) error {
+		// Access the bucket or storage where events are stored
+		bucket := tx.Bucket([]byte("events"))
+		if bucket == nil {
+			// Handle the case where the bucket doesn't exist
+			return nil
+		}
+
+		// Iterate over events and process them
+		err := bucket.ForEach(func(key, value []byte) error {
+			// Assuming you have some deserialization logic here
+			var event Event
+			if err := json.Unmarshal(value, &event); err != nil {
+				// Handle the error (log, return, or take appropriate action)
+				fmt.Printf("Error decoding event: %v\n", err)
+				return nil // Continue iterating
+			}
+
+			// Process the event as needed
+			fmt.Printf("Local Event: %+v\n", event)
+
+			return nil // Continue iterating
+		})
+
+		return err
+	})
+
+	if err != nil {
+		// Handle the error (log, return, or take appropriate action)
+		fmt.Printf("Error fetching local events: %v\n", err)
+	}
+}
+func (*Reverse) prepareConfig() {
+
+}
+
+// Assuming there's a remote server URL to fetch events
+const remoteServerURL = "https://example.com/fetch-events"
+
+func (r *Reverse) remoteFetchEvent() error {
+	// Create a new context with timeout for the HTTP request
+	ctx, cancel := context.WithTimeout(r.ctx, 10*time.Second)
+	defer cancel()
+
+	// Prepare the request
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, remoteServerURL, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+
+	// Set headers, authentication, or any other required settings
+	req.Header.Set("Authorization", "Bearer "+r.config.Token)
+
+	// Send the HTTP request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to fetch events: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the HTTP status code
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	// Decode the response body
+	var eventResponse fetchEventResponse
+	if err := json.NewDecoder(resp.Body).Decode(&eventResponse); err != nil {
+		return fmt.Errorf("failed to decode response: %v", err)
+	}
+
+	// Process the fetched events (use eventResponse.Event)
+	// TODO: Implement your logic for handling the events
+
+	return nil
+}
+*/

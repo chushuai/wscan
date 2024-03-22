f3585033c136cf93a7716139b621a65d1e09fa77
shaochuyu <shaochuyu@360.cn>
2024-03-23 00:15:21 +0800

    【xstream】添加4个xstream相关的插件

--- core/plugins/plugins.go
+++ core/plugins/plugins.go
@@ -6,14 +6,27 @@ package plugins
 
 import (
 	"wscan/core/plugins/base"
+	"wscan/core/plugins/baseline"
+	"wscan/core/plugins/bruteforce"
+	"wscan/core/plugins/cmd_injection"
 	"wscan/core/plugins/crlf_injection"
-	"wscan/core/plugins/custom_tmpl"
+	"wscan/core/plugins/custom"
 	"wscan/core/plugins/dirscan"
+	"wscan/core/plugins/fastjson"
 	"wscan/core/plugins/jsonp"
 	"wscan/core/plugins/path_traversal"
 	"wscan/core/plugins/prometheus"
+	"wscan/core/plugins/redirect"
+	"wscan/core/plugins/shiro"
 	"wscan/core/plugins/sql_injection"
+	"wscan/core/plugins/ssrf"
+	"wscan/core/plugins/struts"
+	"wscan/core/plugins/thinkphp"
+	"wscan/core/plugins/upload"
+	"wscan/core/plugins/waftest"
 	"wscan/core/plugins/xss"
+	"wscan/core/plugins/xstream"
+	"wscan/core/plugins/xxe"
 )
 
 func GetPluginByName(name string) base.Plugin {
@@ -35,6 +48,19 @@ func All() []base.Plugin {
 	plugins = append(plugins, &jsonp.JSONP{})
 	plugins = append(plugins, &dirscan.Dirscan{})
 	plugins = append(plugins, &prometheus.Prometheus{})
-	plugins = append(plugins, &custom_tmpl.CustomTmpl{})
+	plugins = append(plugins, &waftest.CustomTmpl{})
+	plugins = append(plugins, &cmd_injection.CmdInjection{})
+	plugins = append(plugins, &bruteforce.BruteForce{})
+	plugins = append(plugins, &struts.Struts{})
+	plugins = append(plugins, &ssrf.SSRF{})
+	plugins = append(plugins, &thinkphp.Thinkphp{})
+	plugins = append(plugins, &xxe.XXE{})
+	plugins = append(plugins, &redirect.Redirect{})
+	plugins = append(plugins, &fastjson.FastJSON{})
+	plugins = append(plugins, &upload.Upload{})
+	plugins = append(plugins, &baseline.Baseline{})
+	plugins = append(plugins, &shiro.Shiro{})
+	plugins = append(plugins, &custom.Custom{})
+	plugins = append(plugins, &xstream.Xstream{})
 	return plugins
 }
---
+++ core/plugins/xstream/xstream.go
@@ -0,0 +1,62 @@
+/**
+2 * @Author: shaochuyu
+3 * @Date: 1/23/24
+4 */
+
+package xstream
+
+import (
+	"context"
+	"wscan/core/plugins/base"
+	logger "wscan/core/utils/log"
+)
+
+type Config struct {
+	base.PluginBaseConfig `json:",inline" yaml:",inline"`
+}
+
+// BaseConfig 返回基本配置, 固定格式, 无需修改
+func (c *Config) BaseConfig() *base.PluginBaseConfig {
+	return &c.PluginBaseConfig
+}
+
+type Xstream struct {
+	base.PluginMixinInitConfig
+	base.PluginMixinClose
+}
+
+// Close 关闭函数
+func (*Xstream) Close() error {
+	return nil
+}
+
+// DefaultConfig 返回默认配置, 需要填写插件的默认配置
+func (*Xstream) DefaultConfig() base.PluginConfigInterface {
+	config := &Config{PluginBaseConfig: base.PluginBaseConfig{
+		Name:    "xstream",
+		Enabled: true,
+	}}
+	return config
+}
+
+// Fingers 返回漏洞检测配置
+func (p *Xstream) Fingers() []*base.Finger {
+	fingers := []*base.Finger{}
+	fingers = append(fingers, (&CVE_2021_21345{}).Finger())
+	fingers = append(fingers, (&CVE_2020_26259{}).Finger())
+	fingers = append(fingers, (&CVE_2020_26217{}).Finger())
+	fingers = append(fingers, (&CVE_2013_7285{}).Finger())
+
+	return fingers
+}
+
+// GetConfig 获取配置
+func (p *Xstream) GetConfig() base.PluginConfigInterface {
+	return p.PluginMixinInitConfig.GetConfig()
+}
+
+// Init 插件初始化
+func (p *Xstream) Init(ctx context.Context, pci base.PluginConfigInterface, ab *base.ApolloBase) error {
+	logger.Info("xstream init")
+	return p.PluginMixinInitConfig.Init(ctx, pci, ab)
+}

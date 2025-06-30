044936f29ae4b84e419dce189778ddc8cdce5561
shaochuyu <shaochuyu@360.cn>
2025-07-01 00:42:33 +0800

    【CHANGELOG.md】升级到1.0.34

--- core/main.go
+++ core/main.go
@@ -204,7 +204,7 @@ func main() {
 	app := &cli.App{
 		Name:    "wscan",
 		Usage:   "A powerful scanner engine ",
-		Version: "1.0.31",
+		Version: "1.0.34",
 		Authors: []*cli.Author{&author},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
